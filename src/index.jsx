a9e8edfc80d99ba18b65dcd89f2a555fd727d672
kathrynhoule <kathryn.houle@torontomu.ca>
2024-11-15 18:35:00 -0500

    sets up initial files

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = document.getElementById('root');
+if (root) {
+  const rootElement = ReactDOM.createRoot(root);
+  rootElement.render(<App />);
+} else {
+  console.error('Root element not found!');
+}
