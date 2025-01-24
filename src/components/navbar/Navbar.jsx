d7dabb079970e8781a541286d4e1e4224c86ec8f
kathrynhoule <kathryn.houle@torontomu.ca>
2025-01-24 15:06:35 -0500

    adds a closeicon and usestate for opening and closing the menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -4,12 +4,22 @@ import { useState } from "react";
 import { getImageURL } from "../../utils";
 
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
   <nav className={styles.navbar}>
     <a className={styles.title} href="/">Portfolio</a>
     <div className={styles.menu}>
-      <img className={styles.menuBtn} src ={getImageURL("nav/menuIcon.png")} alt="menu-button" />
-      <ul className={styles.menuItems}>
+      <img 
+      className={styles.menuBtn} 
+      src= {
+          menuOpen ? getImageURL("nav/closeIcon.png")
+          : getImageURL("nav/menuIcon.png")
+        } 
+      alt="menu button"
+      onClick = {() => setMenuOpen(!menuOpen)}
+      />
+      <ul className={`${styles.menuItems} ${menuOpen && styles.menuOpen}`}>
         <li><a href="#about">About</a></li>
         <li><a href="#experience">Experience</a></li>
         <li><a href="#projects">Projects</a></li>
