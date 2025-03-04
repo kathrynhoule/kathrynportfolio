38514b873f32ceaa7f16347606c3ef6c010227c0
kathrynhoule <kathryn.houle@torontomu.ca>
2025-03-04 15:35:21 -0500

    updates css for main section

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from './App.module.css';
 import Navbar from "./components/navbar/Navbar";
+import Headline from "./components/headline/Headline";
 
 const App = () => {
      return (
           <div className={styles.App}>
                <Navbar />
+               <Headline />
           </div>
      );
 }
