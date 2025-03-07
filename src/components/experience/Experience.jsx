0ce1f61c8907f3c7bba444d4ae3099e0b6683830
kathrynhoule <kathryn.houle@torontomu.ca>
2025-03-06 20:40:26 -0500

    maps the json data to the experience section

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -1,10 +1,37 @@
 import React from 'react'
 import { getImageURL } from '../../utils';
 import styles from './Experience.module.css';
+import skills from '../../data/skills.json';
+import history from '../../data/history.json';
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <section id="experience">
+     <h2>Experience</h2>
+     <div>
+          <div>
+               {skills.map((skill, id) => {
+                    return (<div key={id}>
+                         <div><img src={getImageURL(skill.imageSrc)} alt={skill.title} />
+                         <p>{skill.title}</p>
+                         </div>
+                    </div>
+                    );
+               })}
+          </div>
+          <ul>
+               {
+                    history.map((historyItem, id) => {
+                         return (
+                              <li key={id}>
+                                   <img src={getImageURL(historyItem.imageSrc)} alt={`$(historyItem.organization) Logo`} />
+                              </li>
+                         )
+                    })
+               }
+          </ul>
+     </div>
+    </section>
   )
 }
 
