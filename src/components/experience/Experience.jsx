e5ad9eb10bc7bd9ae7b49fb8b3af752ecaa71167
kathrynhoule <kathryn.houle@torontomu.ca>
2025-03-07 10:17:13 -0500

    adds classes for styling

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -6,25 +6,36 @@ import history from '../../data/history.json';
 
 const Experience = () => {
   return (
-    <section id="experience">
-     <h2>Experience</h2>
-     <div>
-          <div>
+    <section className={styles.container} id="experience">
+     <h2 className={styles.title}>Experience</h2>
+     <div className={styles.content}>
+          <div className={styles.skills}>
                {skills.map((skill, id) => {
-                    return (<div key={id}>
-                         <div><img src={getImageURL(skill.imageSrc)} alt={skill.title} />
+                    return (
+                         <div className={styles.skill} key={id}>
+                         <div className={styles.skillImgContainer}>
+                              <img src={getImageURL(skill.imageSrc)} alt={skill.title} />
                          <p>{skill.title}</p>
                          </div>
                     </div>
                     );
                })}
           </div>
-          <ul>
+          <ul className={styles.history}>
                {
                     history.map((historyItem, id) => {
                          return (
-                              <li key={id}>
+                              <li className={styles.historyItem} key={id}>
                                    <img src={getImageURL(historyItem.imageSrc)} alt={`$(historyItem.organization) Logo`} />
+                              <div className={styles.historyItemDetails}>
+                                   <h3>{`${historyItem.role}, ${historyItem.organization}`}</h3>
+                                   <p>{`${historyItem.startDate} - ${historyItem.endDate}`}</p>
+                                   <ul>{historyItem.experiences.map((experience, id) => {
+                                        return (
+                                             <li key={id}>{experience}</li>
+                                        )
+                                   })}</ul>
+                              </div>
                               </li>
                          )
                     })
